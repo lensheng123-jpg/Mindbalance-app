bcb815013c8b39d5e9d66d5d88fc516d36a899b6
KuanLi <lensheng123@gmail.com>
2025-09-23 08:52:17 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mindbalance-app',
+  webDir: 'dist'
+};
+
+export default config;
