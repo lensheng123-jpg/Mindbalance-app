52880a12c91fa41f811ec83cac8492a51852a8f0
lensheng123-jpg <lensheng123@gmail.com>
2025-10-04 11:25:42 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,46 @@
-// src/pages/Home.tsx
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/react";
+import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from "@ionic/react";
+import { useHistory } from "react-router-dom";
 import AddMood from "../components/AddMood";
 import MoodList from "../components/MoodList";
+import MoodChart from "../components/MoodChart";
+import MoodPie from "../components/MoodPie";
+import StressTrendChart from "../components/StressTrendChart";
 
-export default function Home() {
+interface Props {
+  userId: string;
+}
+
+export default function Home({ userId }: Props) {
+  const history = useHistory();
+
+  const navigateToMindfulness = () => {
+    history.push('/mindfulness');
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>MindBalance - Mood Tracker</IonTitle>
+          <IonTitle>MindBalance aid - Mood Tracker</IonTitle>
         </IonToolbar>
       </IonHeader>
 
-      <IonContent className="ion-padding">
-        <AddMood userId="userId" />
-        <MoodList userId="userId" />
+    <IonContent className="ion-padding">
+        {/* Add Mindfulness Navigation Button */}
+        <IonButton 
+          expand="block" 
+          onClick={navigateToMindfulness}
+          style={{ marginBottom: '20px' }}
+          color="tertiary"
+        >
+          🧘 Mindfulness Exercises
+        </IonButton>
+
+        <AddMood userId={userId} />
+        <MoodChart userId={userId} />
+        <MoodPie userId={userId} />
+        <MoodList userId={userId} />
+        <StressTrendChart userId={userId} />
       </IonContent>
     </IonPage>
   );
