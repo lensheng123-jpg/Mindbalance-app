6336a6973b2d012cfb7c406ac6107ef8ff5eaaad
lensheng123-jpg <lensheng123@gmail.com>
2025-11-09 10:26:48 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,41 +7,41 @@ import MoodPie from "../components/MoodPie";
 import StressTrendChart from "../components/StressTrendChart";
 
 interface Props {
-  userId: string;
+userId: string;
 }
 
 export default function Home({ userId }: Props) {
-  const history = useHistory();
+const history = useHistory();
 
-  const navigateToMindfulness = () => {
-    history.push('/mindfulness');
-  };
+const navigateToMindfulness = () => {
+history.push('/mindfulness');
+};
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>MindBalance aid - Mood Tracker</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+return (
+<IonPage>
+<IonHeader>
+<IonToolbar>
+<IonTitle>MindBalance aid - Mood Tracker</IonTitle>
+</IonToolbar>
+</IonHeader>
 
-    <IonContent className="ion-padding">
-        {/* Add Mindfulness Navigation Button */}
-        <IonButton 
-          expand="block" 
-          onClick={navigateToMindfulness}
-          style={{ marginBottom: '20px' }}
-          color="tertiary"
-        >
-          🧘 Mindfulness Exercises
-        </IonButton>
+<IonContent className="ion-padding">
+{/* Add Mindfulness Navigation Button */}
+<IonButton
+expand="block"
+onClick={navigateToMindfulness}
+style={{ marginBottom: '20px' }}
+color="tertiary"
+>
+🧘 Mindfulness Exercises
+</IonButton>
 
-        <AddMood userId={userId} />
-        <MoodTrendChart userId={userId} />
-        <MoodPie userId={userId} />
-        <MoodList userId={userId} />
-        <StressTrendChart userId={userId} />
-      </IonContent>
-    </IonPage>
-  );
+<AddMood userId={userId} />
+<MoodTrendChart userId={userId} />
+<MoodPie userId={userId} />
+<MoodList userId={userId} />
+<StressTrendChart userId={userId} />
+</IonContent>
+</IonPage>
+);
 }
